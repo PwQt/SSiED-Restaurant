c58cc5f701910a897e532f410b9a9d1cc84a1aaf
Paweł <pwkucia@gmail.com>
2018-01-08 17:35:13 +0100

    Dodanie przykładowego wykresu + czyszczenie kodu

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import csv
 import sys
-import pandas as pd
-import numpy as np
-import matplotlib as mtpllib
+import pandas as pd 
+import numpy as np #Wymagana zależność dla pandas
+import matplotlib.pyplot as plt # Wykresy
 
-
+#Import danych szkoleniowych z pliku do struktur DataFrames
 air_reserve = pd.read_csv('data/air_reserve.csv', encoding='utf-8')
 air_store_info = pd.read_csv('data/air_store_info.csv', encoding='utf-8')
 air_visit_data = pd.read_csv('data/air_visit_data.csv', encoding='utf-8')
@@ -13,11 +13,14 @@ hpg_reserve = pd.read_csv('data/hpg_reserve.csv')
 hpg_store_info = pd.read_csv('data/hpg_store_info.csv')
 store_id_relation = pd.read_csv('data/store_id_relation.csv')
 
+#Tu jest nasz zbiór testowy z datami
 sample_submission = pd.read_csv('data/sample_submission.csv')
 
+#Łączenie struktur testowych do jednego DataFrame
 joined = air_reserve.set_index('air_store_id').join(air_store_info.set_index('air_store_id'))
 air_data = joined.join(air_visit_data.set_index('air_store_id'))
 
-with pd.option_context('display.max_rows', 10, 'display.max_columns', None):
-    print(air_data)
-    sys.stdin.read(1)
+#Przykładowy wykres uzyskany z istniejących danych
+plt.plot(air_data['visit_date'][0:100], air_data['visitors'][0:100])
+plt.title('Wizyty')
+plt.show()
